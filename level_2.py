3b463d7fecb1d0f84102f544800829a75bb3f3d7
VaamanBansal <vaamanbansal12@gmail.com>
2025-03-03 18:06:45 +0530

    Add files via upload

---
+++ level_2.py
@@ -0,0 +1,20 @@
+class Interval:
+    def __init__(self):
+        self.intervals=[]
+    def add_interval(self,a,b):
+        self.intervals.append([a, b])
+        self.intervals.sort()
+        m= []
+        for i in self.intervals:
+            if not m or m[-1][1] < i[0]:
+                m.append(i) 
+            else:
+                m[-1][1] = max(m[-1][1], i[1])
+        self.intervals = m
+    def get_interval(self):
+        print(self.intervals)
+I1=Interval()
+I1.add_interval(4,5)
+I1.add_interval(7,10)
+I1.add_interval(1,3)
+I1.get_interval()
