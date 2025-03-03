7493c4e12feab0ffa0566b4ec2af0308ada8caf9
VaamanBansal <vaamanbansal12@gmail.com>
2025-03-03 19:14:06 +0530

    Update level_2.py

--- level_2.py
+++ level_2.py
@@ -1,20 +1,20 @@
+from sortedcontainers import SortedList
 class Interval:
     def __init__(self):
-        self.intervals=[]
-    def add_interval(self,a,b):
-        self.intervals.append([a, b])
-        self.intervals.sort()
-        m= []
+        self.intervals = SortedList()
+    def add_interval(self, a, b):
+        self.intervals.add([a, b])
+        m = SortedList()
         for i in self.intervals:
             if not m or m[-1][1] < i[0]:
-                m.append(i) 
+                m.add(i)
             else:
                 m[-1][1] = max(m[-1][1], i[1])
         self.intervals = m
     def get_interval(self):
-        print(self.intervals)
-I1=Interval()
-I1.add_interval(1,5)
-I1.add_interval(7,10)
-I1.add_interval(2,9)
+        print(list(self.intervals))
+I1 = Interval()
+I1.add_interval(1, 5)
+I1.add_interval(7, 10)
+I1.add_interval(2, 9)
 I1.get_interval()
